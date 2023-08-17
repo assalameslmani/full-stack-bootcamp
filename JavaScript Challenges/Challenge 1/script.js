020d1b8c4441bffedf970a93e92ae294eb54068c
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2023-08-17 10:48:41 +0300

    "Challenge 1 - Done with Step 4"

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -10,3 +10,28 @@ age=25;
 lastName="Jean";
 DOB="1998-06-10";
 console.log(age,lastName,DOB);
+
+
+console.log(typeof "this is a text");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof 1215);
+console.log(typeof "999");
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof undefined);
+
+let name="Assala";//string
+let weight = 53;//number
+let student=true;//boolean
+let height=1.68;//number
+let hobbies=["reading","painting","swimming","basketball"];//array
+let address={street:"123 main st",city:"beirut"};//object
+
+console.log("name:", typeof name);
+console.log("weight:", typeof weight);
+console.log("student:", typeof student);
+console.log("height:", typeof height);
+console.log("hobbies:",typeof hobbies);
+console.log("address:", typeof address);
