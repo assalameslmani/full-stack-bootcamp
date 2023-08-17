6136e9453bb2ae75829975275bd4d2143600691d
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2023-08-17 12:02:49 +0300

    "Challenge 2 - Done with step 1".

---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,20 @@
+//step1
+//1
+let age = 22;
+//2
+if (age >= 20) {
+    console.log("You can enter this room.");
+} else {
+    console.log("You can't enter this room.");
+}
+//3
+//4
+const expression1 = 5 > 3;
+const expression2 = 10 === '10';
+const expression3 = true && false;
+const expression4 = !expression3;
+//5
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
