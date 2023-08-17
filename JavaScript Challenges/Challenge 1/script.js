79d6978df7d943ccdb91e1bcd48957237a857e08
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2023-08-17 11:10:44 +0300

    "Challenge 1 - Done with Step 5"

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,8 +1,11 @@
 console.log('I am linked now!');
+
+//step2
 var name;
 let name="Assala";
 alert (Assala);
 
+//step3
 let age=23;
 var lastName="Meslmani";
 const DOB="2000-10-10";
@@ -11,7 +14,7 @@ lastName="Jean";
 DOB="1998-06-10";
 console.log(age,lastName,DOB);
 
-
+//step4
 console.log(typeof "this is a text");
 console.log(typeof true);
 console.log(typeof false);
@@ -35,3 +38,25 @@ console.log("student:", typeof student);
 console.log("height:", typeof height);
 console.log("hobbies:",typeof hobbies);
 console.log("address:", typeof address);
+
+//step5
+let myText ="Hello, how are you?"
+
+let text1="this is a string using double quotations";//double quot
+
+let text2='this is a string using single quotation'; //single quot
+
+let text3='this is a sting using backticks (template literals)';//backticks
+
+const counter = 3;
+const backtick = `This is my text using backticks ${counter} times`;
+const singleQuote = `This is my text using single quotations ${counter} times`;
+const doubleQuote = `This is my text using double quotations ${counter} times`;
+const tryThisAlso = `This is my new version, it's the ${2 + 3}th version`;//In these examples, the ${counter} placeholder is replaced with the value of the counter variable, and the ${2 + 3} placeholder is replaced with the result of the expression 2 + 3. This way, you can dynamically include variables and expressions within your strings. Well done!
+
+console.log("backtick:", backtick);
+console.log("singleQuote:", singleQuote);
+console.log("doubleQuote:", doubleQuote);
+console.log("tryThisAlso:", tryThisAlso);
+//This will log the values of all the variables (`backtick`, singleQuote, doubleQuote, `tryThisAlso`) to the console.
+//backticks (template literals) are the most versatile as they support string interpolation and multi-line strings. Single and double quotations are more basic and do not support string interpolation.
