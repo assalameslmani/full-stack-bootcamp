875b00f0ee0edbcaef517cef2efa9f3d516c08e6
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2023-08-22 15:10:31 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -2,15 +2,34 @@
 
 //step 1
 
-let fiveNumbers = [1,2,3,4,5];
+let fiveNumbers = [1, 2, 3, 4, 5];
 console.log(fiveNumbers);
 
 let personalInfo = {
-    name:"jane smith",
-    age:30,
-    height:"178 cm",
-    location:"los angeles",
-    nationality:"canadian"
+    name: "jane smith",
+    age: 30,
+    height: "178 cm",
+    location: "los angeles",
+    nationality: "canadian"
 };
 console.log(personalInfo);
 
+
+//step 2 
+
+let nextThreeNumbers = [6, 7, 8];
+let allNumbers =
+fiveNumbers.concat(nextThreeNumbers);
+//concat: used to combine the fivenumbers array with the nextthreenumbers.
+console.log(nextThreeNumbers);
+console.log(allNumbers);
+
+let additionalInfo = {
+    occupation:"software enginner",
+    hobby:"reading",
+    education:"bachelor's degree",
+};
+console.log(additionalInfo);
+
+let fullInfo = {...personalInfo, ...additionalInfo};
+console.log(fullInfo);
