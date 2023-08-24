197e95e3ad60176e2ca108755b13c3899c4b90ab
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2023-08-24 15:10:53 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -21,3 +21,33 @@ button.addEventListener("click", function() {
   button.style.color = "white";
   button.textContent = "Clicked " + randomNumber + "!";
 });
+
+
+
+//step 2
+// Create h1 element
+const h1 = document.createElement("h1");
+
+// Add the h1 element to the body of the page
+document.body.appendChild(h1);
+
+// Add border to the h1 element
+h1.style.border = "10px dotted green";
+
+// Add keydown event listeners
+document.addEventListener("keydown", function(event) {
+  if (event.key === "a") {
+    h1.style.backgroundColor = "red";
+  } else if (event.key === "s") {
+    h1.style.left = (parseInt(h1.style.left) || 0) + 10 + "px";
+  } else if (event.key === "d") {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "Key D was pressed!";
+    document.body.appendChild(paragraph);
+  } else if (event.key === "w") {
+    h1.style.visibility = h1.style.visibility === "visible" ? "hidden" : "visible";
+  } else if (event.key === " ") {
+    const currentFontSize = parseInt(window.getComputedStyle(h1).fontSize);
+    h1.style.fontSize = currentFontSize + 1 + "px";
+  }
+});
