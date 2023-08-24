bb339d1c4930e2275e70b455f92d30095df37e61
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2023-08-24 15:15:41 +0300

    Challenge 5.1 - Done with step 3

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -51,3 +51,48 @@ document.addEventListener("keydown", function(event) {
     h1.style.fontSize = currentFontSize + 1 + "px";
   }
 });
+
+
+
+//step 3
+const form = document.getElementById("myForm");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const confirmPassword = confirmPasswordInput.value;
+
+  if (!isValidEmail(email)) {
+    alert("Please enter a valid email address.");
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match.");
+    return;
+  }
+
+  alert("Form submitted successfully!");
+});
+
+function isValidEmail(email) {
+  // Use a regular expression to validate the email address
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+const inputs = document.querySelectorAll("input");
+inputs.forEach(function(input) {
+  input.addEventListener("focus", function() {
+    this.style.backgroundColor = "lightblue";
+  });
+
+  input.addEventListener("blur", function() {
+    this.style.backgroundColor = "";
+  });
+});
