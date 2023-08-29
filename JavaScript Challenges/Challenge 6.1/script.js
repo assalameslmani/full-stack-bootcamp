2051e4efcb70f494566adabd5f37ba5b4e89f7ec
assalameslmani <139454177+assalameslmani@users.noreply.github.com>
2023-08-29 10:00:22 +0300

    Challenge 6.1 - Done with Step 1

---
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -0,0 +1,16 @@
+//step1
+function delay (milliseconds){
+    return new Promise(resolve => setTimeout (resolve,milliseconds));
+}
+
+
+//step2
+fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+.then(response=>response.json())
+.then(data =>{
+    const randomFact= data.text;
+    console.log(randomFact);
+})
+
+.catch(error => {console.log ("An error occured while fetching the random fact.");
+});
